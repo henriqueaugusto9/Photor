fddc83afeeb2829a18fbc7f1b622de4c88805333
Lucas Daniel Pedroso <lukas_boo@hotmail.com>
2020-08-21 17:50:09 -0300

    implementing loading

--- Photor/Controllers/MainViewController.swift
+++ Photor/Controllers/MainViewController.swift
@@ -5,25 +5,38 @@ import UnsplashPhotoPicker
 class MainViewController: UIViewController {
     
     var viewModel = PhotosViewModel()
+    let activityView = UIActivityIndicatorView(style: .large)
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var photosTableView: UITableView!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
+        super.viewDidLoad()                
         if let url = URL(string: "\(Constants.API_URL_BASE)") {
             getPhotos(url: url, search: false)
+        } else {
+            self.showAlert(title: "Erro", message: "Ocorreu um erro, tente mais tarde novamente!")
         }
     }
     
     func getPhotos(url: URL, search: Bool) {
         self.viewModel.getPhotos(url: url, search: search) { [weak self] results in
-            print(results)
+            self!.showLoading(show: true)
             DispatchQueue.main.async {
                 self?.photosTableView.reloadData()
+                self!.showLoading(show: false)
             }
+        }
+    }
+    
+    func showLoading(show: Bool) {
+        if show {
+            self.activityView.center = self.view.center
+            self.activityView.startAnimating()
+            self.view.addSubview(activityView)
+        } else {
+            self.activityView.stopAnimating()
+            self.activityView.removeFromSuperview()
         }
     }
         
@@ -34,7 +47,8 @@ extension MainViewController: UISearchBarDelegate {
         
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         if !searchBar.text!.isEmpty {
-            if let url = URL(string: "https://api.unsplash.com/search/photos?page=1&query=\(searchBar.text!)&client_id=EGtTvOh6MlWtTDYKfxwR02BWWuFRd2b2AsDvbFCXqxI") {                self.getPhotos(url: url, search: true)
+            if let url = URL(string: "https://api.unsplash.com/search/photos?page=1&query=\(searchBar.text!)&client_id=EGtTvOh6MlWtTDYKfxwR02BWWuFRd2b2AsDvbFCXqxI") {
+                self.getPhotos(url: url, search: true)
             }
         }
     }
